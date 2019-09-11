586e1137b4481298815d8ca4debb43cc4f6ebfde
aesthetiffs <aesthetiffs@users.noreply.github.com>
2019-09-11 00:15:33 -0500

    Create sidebar.js
    for hiding/revealing the sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,20 @@
+/* Set the width of the side navigation to 250px */
+// later need to make this more dynamic by changing the size of the div for each breakpoint rather than the width 
+
+function openNav() {
+  if (window.matchMedia("(max-width: 992px)").matches) {
+	document.getElementById("sidebarMenu").style.width = "100vw";
+  }
+  else {
+    document.getElementById("sidebarMenu").style.width = "35vw";
+  }
+  document.getElementById("main-left").style.backgroundColor = "rgba(0, 0 , 0, .3)";
+  document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, .3)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sidebarMenu").style.width = "0";
+  document.getElementById("main-left").style.backgroundColor = "#fbfbfb";
+  document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, 0)";
+}
