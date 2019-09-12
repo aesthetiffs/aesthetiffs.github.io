1dd9b24f697ca3867b8677d6bbb8218d3fd309cd
Tiff N <aesthetiffs@users.noreply.github.com>
2019-09-11 21:33:33 -0500

    minor fixes to subtitle size and background when sidebar is open

--- js/sidebar.js
+++ js/sidebar.js
@@ -10,6 +10,7 @@ function openNav() {
   }
   document.getElementById("main-left").style.backgroundColor = "rgba(0, 0 , 0, .3)";
   document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, .3)";
+  document.getElementById("main-other").style.backgroundColor = "rgba(0, 0 , 0, .3)";
 }
 
 /* Set the width of the side navigation to 0 */
@@ -17,4 +18,5 @@ function closeNav() {
   document.getElementById("sidebarMenu").style.width = "0";
   document.getElementById("main-left").style.backgroundColor = "#fbfbfb";
   document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, 0)";
+  document.getElementById("main-other").style.backgroundColor = "rgba(0, 0 , 0, 0)";
 }
