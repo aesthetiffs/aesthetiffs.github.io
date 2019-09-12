cea03127f04e0056adf3646ce026bc76d7c12785
Tiff N <aesthetiffs@users.noreply.github.com>
2019-09-11 21:37:18 -0500

    minor fixes to background when sidebar is open

--- js/sidebar-content.js
+++ js/sidebar-content.js
@@ -8,15 +8,15 @@ function openNav() {
   else {
     document.getElementById("sidebarMenu").style.width = "35vw";
   }
-  document.getElementById("main-left").style.backgroundColor = "rgba(0, 0 , 0, .3)";
-  document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, .3)";
-  // document.getElementById("main-other").style.backgroundColor = "rgba(0, 0 , 0, .3)";
+  // document.getElementById("main-left").style.backgroundColor = "rgba(0, 0 , 0, .3)";
+  // document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, .3)";
+  document.getElementById("main-other").style.backgroundColor = "rgba(0, 0 , 0, .3)";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("sidebarMenu").style.width = "0";
-  document.getElementById("main-left").style.backgroundColor = "#fbfbfb";
-  document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, 0)";
-  // document.getElementById("main-other").style.backgroundColor = "rgba(0, 0 , 0, 0)";
+  // document.getElementById("main-left").style.backgroundColor = "#fbfbfb";
+  // document.getElementById("main-right").style.backgroundColor = "rgba(0, 0 , 0, 0)";
+  document.getElementById("main-other").style.backgroundColor = "rgba(0, 0 , 0, 0)";
 }
